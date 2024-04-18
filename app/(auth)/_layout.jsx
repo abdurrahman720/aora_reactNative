89405c34168ff7d492b9beb05031b18e264e5208
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-04-18 18:45:24 +0600

    login screen

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {  StyleSheet, Text, View } from 'react-native'
+import { Stack, SplashScreen } from "expo-router";
+import {StatusBar} from 'expo-status-bar';
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{
+          headerShown:false
+        }} />
+        <Stack.Screen name="sign-up" options={{
+          headerShown:false
+        }} />
+      </Stack>
+      <StatusBar backgroundColor='#161622' style='light'/>
+    </>
   )
 }
 
